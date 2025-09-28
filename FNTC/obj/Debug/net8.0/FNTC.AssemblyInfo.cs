6ddc250f1343ae266c4600db2d7444d7ae2a7208
Adrn00 <adrianfirst09@gmail.com>
2025-09-28 16:39:42 +0800

    main window updated (redesign side bar, header, dashb panel, profilenotif panel, 3 border main content

--- FNTC/obj/Debug/net8.0/FNTC.AssemblyInfo.cs
+++ FNTC/obj/Debug/net8.0/FNTC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FNTC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65db21af4fdbf034eeabbf29982d63cced54fec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4053e330c53b3ec9ef514d591c3118c55c45c03e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FNTC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FNTC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
