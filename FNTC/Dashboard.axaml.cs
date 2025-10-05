c5b23d2438cce82158b7aa7a3deb59cec069ba3e
Adrn00 <adrianfirst09@gmail.com>
2025-10-05 12:21:48 +0800

    donut chart

--- FNTC/Dashboard.axaml.cs
+++ FNTC/Dashboard.axaml.cs
@@ -1,12 +1,13 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
-using System.Collections.Generic;
 
 namespace FNTC
 {
     public partial class Dashboard : Window
     {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
     }
 }
